b7a438ff072800ca20e52d8872781992c57233a7
spaquet <stephan.paquet@duproprio.com>
2015-11-09 08:35:31 -0500

    Add lesson 2.4

---
+++ Comparing-Front-End-Frameworks/2.4-Backbone-Contact-List-View/README.md
@@ -0,0 +1,5 @@
+# Backbone Router
+In this lesson we will build the router, where we define which routes our application will use. The router connects URLs to JavaScript functions and ultimately to Backbone.Views.
+
+### Related Links
+- [Backbone.Router](http://backbonejs.org/#Router)
---
+++ Comparing-Front-End-Frameworks/2.4-Backbone-Contact-List-View/backbone/public/app.js
@@ -0,0 +1,86 @@
+/*global Backbone */
+
+// Model
+var Contact = Backbone.Model.extend({});
+
+// Collection
+var Contacts = Backbone.Collection.extend({
+    model: Contact,
+    url: '/api/contacts'
+});
+
+// Views
+var ContactsListView = Backbone.View.extend({
+    tagName: 'ul',
+    render: function () {
+        this.collection.forEach(function (model) {
+            this.$el.append((new ContactsItemView({model: model})).render().el);
+        }, this);
+
+        return this;
+    }
+});
+
+var ContactsItemView = Backbone.View.extend({
+    tagName: 'li',
+    events: {
+        'click a': 'handleClick'
+    },
+    template: _.template($('#contacts-item-view-template').html()),
+    render: function () {
+        console.info(this.model.toJSON());
+        this.el.innerHTML = this.template(this.model.toJSON());
+        return this;
+    },
+    handleClick: function (evt) {
+        evt.preventDefault();
+        Backbone.history.navigate('contacts/' + this.model.get('id'), {trigger: true});
+    }
+});
+
+var ActionsView = Backbone.View.extend({
+    className: 'actions',
+    template: '<a href="/contacts/new"> New Contact </a>',
+    render: function () {
+        this.el.innerHTML = this.template;
+        return this;
+    }
+});
+
+// Router
+var Router = Backbone.Router.extend({
+    initialize: function (options) {
+        this.contacts = options.contacts;
+        this.el       = $('#app');
+    },
+    routes: {
+        '': 'main',
+        'contacts': 'displayList',
+        'contacts/new': 'displayForm',
+        'contacts/:id': 'displaySingle'
+    },
+    main: function () {
+        Backbone.history.navigate('contacts', {trigger: true});
+    },
+    displayList: function () {
+        this.el.empty()
+        .append(new ActionsView().render().el)
+        .append(new ContactsListView({ collection: this.contacts}).render().el);
+    },
+    displayForm: function () {
+        console.log('displayForm');
+    },
+    displaySingle: function (id) {
+
+        console.log(this.contacts.get(parseInt(id)).toJSON());
+    }
+});
+
+var contacts = new Contacts();
+
+contacts.fetch().then(function () {
+    var r = new Router({
+        contacts: contacts
+    });
+    Backbone.history.start({ pushState: true});
+});
