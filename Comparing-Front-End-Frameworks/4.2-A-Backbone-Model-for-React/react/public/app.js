f3dc02c8d82af846aa2049addbc5458468add30f
spaquet <stephan.paquet@duproprio.com>
2015-11-10 13:25:59 -0500

    Add lesson 4.2

---
+++ Comparing-Front-End-Frameworks/4.2-A-Backbone-Model-for-React/react/public/app.js
@@ -0,0 +1,12 @@
+// Model - Same as Backbone
+
+// Collection
+var ContactStore = Backbone.Collection.extend({
+    url: '/api/contacts'
+});
+
+var contactstore = new ContactStore();
+
+contactstore.fetch().then(function () {
+
+});
