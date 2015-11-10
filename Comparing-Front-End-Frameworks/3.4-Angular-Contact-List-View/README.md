c426965e5b0cea1b5ac8fbb7d7164cd876a532ec
spaquet <stephan.paquet@duproprio.com>
2015-11-10 07:48:59 -0500

    Add lesson 3.4

---
+++ Comparing-Front-End-Frameworks/3.4-Angular-Contact-List-View/README.md
@@ -0,0 +1,7 @@
+# Angular Contact List View
+
+Now we can create a view to display the list of contact records on the main page of our application.
+
+## Related Links
+
+- [AngularJS Developer Guide: Templates](https://docs.angularjs.org/guide/templates)
---
+++ Comparing-Front-End-Frameworks/3.4-Angular-Contact-List-View/angular/public/app.js
@@ -0,0 +1,40 @@
+/*global angular */
+
+var app = angular.module('ContactsApp', ['ngRoute', 'ngResource']);
+
+// Router
+// $routeProvider comes from the ngRoute component
+app.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/contacts', {
+            controller: 'ListController',
+            templateUrl: 'views/list.html'
+        })
+        .when('/contacts/new', {
+            controller: 'NewController',
+            templateUrl: 'views/new.html'
+        })
+        .when('/contacts/:id', {
+            controller: 'SingleController',
+            templateUrl: 'views/single.html'
+        })
+        .otherwise({
+            redirectTo: '/contacts'
+        });
+
+    // use the browser history API
+    // @see https://css-tricks.com/using-the-html5-history-api/
+    $locationProvider.html5Mode(true);
+});
+
+// Contacts Model
+app.factory('Contacts', function ($resource) {
+    return $resource('/api/contacts/:id', {id: '@id'}, {
+        'update': { method: 'PUT'} // overwrite the default GET method on update
+    });
+});
+
+// Controllers
+app.controller('ListController', function ($scope, Contacts) {
+    $scope.contacts = Contacts.query();  // Made a GET request to the API to get all contacts
+});
